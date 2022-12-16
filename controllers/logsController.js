d877f9becfcedc49f29562eac72c51133a4bcb53
Fadila-Ali <fadilaali@pursuit.org>
2022-12-16 13:50:43 -0500

    1 failing

---
+++ controllers/logsController.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const log = express.Router();
+const logsArray = require("../models/log");
+const validateURL = (req, res, next) => {
+  console.log(
+    "This function checks the validity of the POST entered by the user"
+  );
+  next();
+};
+
+// BOOKMARKS ROUTE
+log.get("/", (req, res) => {
+  res.json(logsArray);
+});
+
+// SHOW
+log.get("/:arrayIndex", (req, res) => {
+  const { arrayIndex } = req.params;
+  if (logsArray[arrayIndex]) {
+    res.status(200).json(logsArray[arrayIndex]);
+  } else {
+    res.status(404).json({ error: "Not Found" });
+  }
+});
+
+// logs endpoint for POST
+log.post("/", validateURL, (req, res) => {
+  logsArray.push(req.body);
+  res.json(logsArray.at(-1));
+});
+
+// logs endpoint for Delete
+log.delete("/:id", (req, res) => {
+  logsArray.pop(req.body);
+  res.json(logsArray.at(0));
+});
+
+module.exports = log;
