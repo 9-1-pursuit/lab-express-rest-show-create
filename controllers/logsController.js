f6b522b2c6c7755bc7d49514b9d4b0fe1bcc402a
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-12-16 14:45:22 -0500

    working on bonus part 1

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -2,8 +2,59 @@ const express = require("express");
 const logs = express.Router();
 const logsArr = require("../models/log");
 
+const orderArr = (arr, mode, key, key2) => {
+  if (mode === "asc") {
+    arr.sort((a, b) => {
+      let valA = a[key].toLowerCase();
+      let valB = b[key].toLowerCase();
+      if (valA < valB) {
+        return -1;
+      }
+      if (valA > valB) {
+        return 1;
+      }
+      valA = a[key2].toLowerCase();
+      valB = b[key2].toLowerCase();
+      if (valA < valB) {
+        return -1;
+      }
+      if (valA > valB) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
+  if (mode === "desc") {
+    arr.sort((a, b) => {
+      let valA = a[key].toLowerCase();
+      let valB = b[key].toLowerCase();
+      if (valA > valB) {
+        return -1;
+      }
+      if (valA < valB) {
+        return 1;
+      }
+      valA = a[key2].toLowerCase();
+      valB = b[key2].toLowerCase();
+      if (valA > valB) {
+        return -1;
+      }
+      if (valA < valB) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+};
+
 logs.get("/", (req, res) => {
-  res.status(200).json(logsArr);
+  const response = [...logsArr];
+  const { order, mistakes, lastCrisis } = req.query;
+  if (order) {
+    orderArr(response, order, "captainName", "title");
+  }
+  res.status(200).json(response);
 });
 
 module.exports = logs;
