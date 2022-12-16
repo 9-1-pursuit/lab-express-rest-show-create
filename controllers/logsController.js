09b5ab2e7e19853c8c128d5e025d2706239594c6
TonyH98 <tonyhoang@pursuit.org>
2022-12-16 13:59:55 -0500

    Delete and Update added

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -12,6 +12,9 @@ logs.post("/", (req ,res) => {
     res.json(logsArray[logsArray.length - 1])
 })
 
+
+
+
 logs.get("/:index", (req , res) => {
     const {index} = req.params
     if(logsArray[index]){
@@ -21,5 +24,18 @@ logs.get("/:index", (req , res) => {
         res.status(404).redirect("/logs")
     }
 })
+
+logs.delete("/:index", (req , res) => {
+    const {index} = req.params
+    const deleteLogs = logsArray.splice(index , 1)
+    res.status(200).json(deleteLogs)
+})
+
+logs.put("/index", (req , res) => {
+    const {index} = req.params
+    logsArray[index] = req.body
+    res.status(200).json(logsArray[index])
+})
+
 
 module.exports = logs
