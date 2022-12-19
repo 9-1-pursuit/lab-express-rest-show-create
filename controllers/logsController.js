044ded51950e47005163ef12c3c52a6347265614
bowersjuan <bowersjuan@gmail.com>
2022-12-19 01:21:37 -0500

    Add post functionality to logsController.js

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const logs = express.Router();
-const logsArray = require("../models/log");
+let logsArray = require("../models/log");
 
 logs.get("/", (req, res) => {
   res.send(logsArray);
+});
+
+logs.post("/", (req, res) => {
+  console.log(req.body);
+  logsArray = [...logsArray, req.body];
+  res.send(logsArray.at(-1));
 });
 
 module.exports = logs;
