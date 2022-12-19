54694209b6e5b8f11c582eba3fe2da4855e70cfd
bowersjuan <bowersjuan@gmail.com>
2022-12-19 09:48:23 -0500

    Update GET index and add PUT route to logsController.js

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -6,16 +6,29 @@ logs.get("/", (req, res) => {
   res.send(logsArray);
 });
 
-logs.get("/:id", (req, res) => {
-  const { id } = req.params;
-  logsArray[id]
-    ? res.send(logsArray[id])
-    : res.status(301).send(`Error: Sorry, can't find log item at index: ${id}`);
+logs.get("/:index", (req, res) => {
+  const { index } = req.params;
+  logsArray[index]
+    ? res.send(logsArray[index])
+    : res
+        .status(301)
+        .send(`Error: Sorry, can't find log item at index: ${index}`);
 });
 
 logs.post("/", (req, res) => {
   logsArray = [...logsArray, req.body];
   res.send(logsArray.at(-1));
+});
+
+logs.put("/index", (req, res) => {
+  const { index } = req.params;
+  if (logsArray[index]) {
+    logsArray[index] = req.body;
+    res.send(logsArray[index]);
+  } else
+    res
+      .status(404)
+      .send(`Error: Sorry, can't find log item at index: ${index}`);
 });
 
 module.exports = logs;
