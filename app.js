aa61f9536a070f47306192350cfbbff0fdf67804
bowersjuan <bowersjuan@gmail.com>
2022-12-19 00:20:26 -0500

    Update app.js and server.js

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
-
 require("dotenv").config();
-morgan("tiny");
+
+app.use(morgan("tiny"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("App running yeah okay...");
+  res.send("welcome to the");
 });
 
 module.exports = app;
