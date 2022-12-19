0d4ecfe91ad3b74e3b0faa4c2b2106e17d45bfe5
Salinamalek <salinamalek@pursuit.org>
2022-12-19 16:01:01 -0500

    bonus completed

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,13 +1,62 @@
 const express = require("express");
 const log = express.Router();
-const logArray = require("../models/log.js");
+const logArray = require("../models/log");
 
 log.get("/", (req, res) => {
-  res.json(logArray);
+  const { order, mistakes, crisis } = req.query;
+
+  if (order === "asc") {
+    const ascend = logArray.sort((a, b) => {
+      if (a.captainName.charAt(0) < b.captainName.charAt(0)) {
+        return -1;
+      } else if (a.captainName.charAt(0) > b.captainName.charAt(0)) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    res.json(ascend);
+  } else if (order === "desc") {
+    const descend = logArray.sort((a, b) => {
+      if (a.captainName.charAt(0) > b.captainName.charAt(0)) {
+        return -1;
+      } else if (a.captainName.charAt(0) < b.captainName.charAt(0)) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    res.json(descend);
+  } else if (mistakes === "true") {
+    const mistake = logArray.filter((a) => a.mistakesWereMadeToday === true);
+    res.json(mistake);
+  } else if (mistakes === "false") {
+    const notMistake = logArray.filter(
+      (b) => b.mistakesWereMadeToday === false
+    );
+    res.json(notMistake);
+  } else if (crisis === "gte20") {
+    const twentyDays = logArray.filter((c) => c.daysSinceLastCrisis >= 20);
+    res.json(twentyDays);
+  } else if (crisis === "gt10") {
+    const tenDays = logArray.filter((a) => a.daysSinceLastCrisis > 10);
+    res.json(tenDays);
+  } else if (crisis === "lte5") {
+    const fiveDays = logArray.filter((a) => a.daysSinceLastCrisis <= 5);
+    res.json(fiveDays);
+  } else {
+    res.json(logArray);
+  }
+});
+
+log.post("/", (req, res) => {
+  logArray.push(req.body);
+  res.json(logArray[logArray.length - 1]);
 });
 
 log.get("/:index", (req, res) => {
   const { index } = req.params;
+  const { a, b } = req.query;
   if (logArray[index]) {
     res.status(200).json(logArray[index]);
   } else {
@@ -15,21 +64,15 @@ log.get("/:index", (req, res) => {
   }
 });
 
-log.post("/", (req, res) => {
-  logArray.push(req.body);
-  res.json(logArray.at(-1));
-});
-
-log.delete("/:index", (req, res) => {
+log.delete("/:id", (req, res) => {
   logArray.pop(req.body);
-  res.json(logArray.at({ index }));
+  res.json(logArray.at({ id }));
 });
 
-//BONUS
-log.put("/:index", (req, res) => {
-  if (logArray[req.params.index]) {
-    logArray[req.params.index] = req.body;
-    res.status(200).json(logArray[req.params.index]);
+log.put("/:id", (req, res) => {
+  if (logArray[req.params.id]) {
+    logArray[req.params.id] = req.body;
+    res.status(200).json(logArray[req.params.id]);
   } else {
     res.status(404).json({ error: "Not Found" });
   }
