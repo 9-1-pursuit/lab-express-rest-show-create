3be2363934ae96682922d4f474d9000cdf2ada4e
minagrullon <minagrullon@pursuit.org>
2022-12-20 18:38:17 -0500

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const logsController = require("./controllers/logsController");
 const app = express();
+const cors = require("cors");
 
 //use
 app.use(express.json());
+app.use(cors());
 app.use("/logs", logsController);
 
 //routes
