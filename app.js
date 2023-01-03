72c32c4a921da89694463be92e751bd8eb5cf2f7
TrianePeart <TrianePeart@gmail.com>
2023-01-03 11:46:27 -0500

    Error fix

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require("cors");
 
 const logController = require('./controllers/logController')
 
 app.use(express.json())
 
 app.use('/logs', logController)
+
+app.use(cors());
 
 module.exports = app
