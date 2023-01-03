7d2bf944d9925a5421082392a6e768933c0b9ab0
Jonathan-Gaal <jonathangaal@pursuit.org>
2023-01-03 16:03:23 -0500

    tigtened up error handling in logs conroller

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
+
 const logsController = require("./controllers/logsController");
 const v2logsController = require("./v2/controllers/v2logsController");
 app.use(express.json());
+app.use(cors());
 app.use(morgan("tiny"));
 app.use("/logs", logsController);
 app.use("/v2/logs", v2logsController);
@@ -12,12 +15,8 @@ app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
-app.get("/error", (req, res) => {
-  res.send("404 Page not found");
-});
-
 app.get("*", (req, res) => {
-  res.status(404).send("404 Page not found");
+  res.status(404).send("404 Page not found!");
 });
 
 module.exports = app;
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -3,13 +3,8 @@ logs = express.Router();
 const logsArray = require("../models/log");
 const { validateURL, validatePost } = require("../models/logsValidation");
 
-// const validateURL = (req, res, next) => {
-//   console.log("This function runs on the POST bookmark");
-//   next();
-// };
-
 logs.get("/", (req, res) => {
-  res.json(logsArray);
+  res.status(200).json(logsArray);
 });
 
 logs.get("/:i", (req, res) => {
@@ -25,9 +20,7 @@ logs.post("/", validateURL, validatePost, (req, res) => {
 logs.delete("/:i", (req, res) => {
   const { i } = req.params;
   const deletedLog = logsArray.splice(i, 1);
-  res.status(200)
-    ? res.json(deletedLog)
-    : res.status(400).json({ error: "Not Found" });
+  res.status(200) ? res.json(deletedLog) : res.status(400).redirect("/*");
 });
 
 logs.put("/:i", validatePost, (req, res) => {
@@ -37,12 +30,8 @@ logs.put("/:i", validatePost, (req, res) => {
     logsArray[i] = req.body;
     res.status(200).json(logsArray[i]);
   } else {
-    res.status(404).json({ error: "Not Found" });
+    res.status(404).redirect("/*");
   }
 });
-
-// logs.all("*", (req, res) => {
-//   res.status(404).send("404 Page not found");
-// });
 
 module.exports = logs;
