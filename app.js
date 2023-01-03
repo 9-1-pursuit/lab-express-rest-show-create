eb292b324395ff4f895b0c396d9cda1152331811
TrianePeart <TrianePeart@gmail.com>
2023-01-03 12:41:46 -0500

    Some errors corrected

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const logController = require('./controllers/logController')
 
 app.use(express.json())
 
-app.use('/logs', logController)
 
 app.use(cors());
+app.use('/logs', logController)
 
 module.exports = app
