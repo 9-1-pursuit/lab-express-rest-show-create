5aedcebb12b2dc91077f073a3d48d39a580fa8d2
PESolut <jahaadpetty@pursuit.org>
2023-01-23 08:32:56 -0500

    single show route created for logs/ ( logsController.js )

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -7,4 +7,14 @@ logs.get("/", (req, res) => {
     res.json(logsArray)
 })
 
+logs.get("/:arrayIndex", (req, res) => {
+    try {
+        const { arrayIndex } = req.params
+        res.status(200).json(logsArray[arrayIndex])
+    } catch (error) {
+        return res.status(404).json(error)
+    }
+  
+})
+
 module.exports = logs
